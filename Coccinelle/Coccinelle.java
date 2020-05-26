11825d944d61362148ec82b3ae19e691bb146f53
blackjack-nix <theo.peresse-gourbil@edu.esiee.fr>
2020-05-26 12:45:54 +0200

    ca marche, a jouter doc + fonctionnalités

--- Coccinelle/Coccinelle.java
+++ Coccinelle/Coccinelle.java
@@ -1,6 +1,7 @@
 // programme pour la partie 1
 
 import java.util.Scanner;
+import java.lang.Math;
 
 
 
@@ -9,23 +10,41 @@ public class Coccinelle {
 
 	// Méthode principale qui se contente d'appeler d'autres fonctions
 	public static void main (String[] args){
-		int[][] grille = {{2,4,3,9,6},{1,10,15,1,2},{2,4,11,26,66},{36,34,1,13,30},{46,2,8,7,15},{89,27,10,12,3},{1,72,3,6,6},{3,1,2,4,5}};
+		
+		int[][] grille = {
+			{2,4,3,9,6},
+			{1,10,15,1,2},
+			{2,4,11,26,66},
+			{36,34,1,13,30},
+			{46,2,8,7,15},
+			{89,27,1,12,3},
+			{1,72,3,6,6},
+			{3,1,2,4,5}};
+
 		printGrille(grille);
 		int M[][] = calculGrille(grille);
-		System.exit(1);
+		printGrille(M);
+		System.exit(0);
 	}
 	
+
 	//calcul le tableau M[c][l] de termes général m(c,l)
 	public static int[][] calculGrille(final int[][] grille){
-		int[][] M = new int[5][8];	
+
+		int[][] M = new int[8][5];	
+	
 		//cas initial
 		for(int c=0; c < 5; c++){M[0][c]=grille[0][c];}
 
 		//cas général
-		for (int l = 7 ; l > 0 ; l++){
-			
-			for(int c = 7; c > 0; c++){
-				M[l][c] = grille[l][c] + max3(M[l-1][c],M[l-1][c-1],M[l-1][c+1]);
+		for (int l = 1 ; l < M.length ; l++){		
+			for(int c = 0 ; c < M[0].length; c++){
+
+				if(c == M[0].length-1) M[l][c] = grille[l][c] + max2(M[l-1][c],M[l-1][c-1]);
+
+				else if (c == 0) M[l][c]=grille[l][c]+max2(M[l-1][c],M[l-1][c+1]);
+
+				else M[l][c] = grille[l][c] + max3(M[l-1][c-1],M[l-1][c],M[l-1][c+1]);
 			}
 		}
 		return M;
@@ -40,20 +59,33 @@ public class Coccinelle {
 			}
 			System.out.println("");
 		}
+		System.out.println("");
 	}
 
 	
 	public static int max3(final int n1, final int n2, final int n3){
-		if( n1 > n2 && n1 > n3) return n1;
-		if( n2 > n1 && n2 > n3) return n2;
-		if( n3 > n1 && n3 > n2) return n3;
-		return 0;
+		return Math.max(n1,Math.max(n2,n3));
 	}
 
+	public static int max2(final int n1, final int n2){
+		return Math.max(n1,n2);
+	}
 
+	public static int[] maxLigne(final int[] ligne){
+		
+		int[] retour = new int[2];
+		int maxLigne = 0, positionLigne = 0;
 
-
-
-
+		for(int i=0; i<ligne.length; i++){
+			if(ligne[i] > maxLigne){
+				maxLigne = ligne[i];
+				positionLigne = i;
+			}
+		}
+		retour[0]=maxLigne;
+		retour[1]=positionLigne;
+		return retour;
+		
+	}
 
 }
