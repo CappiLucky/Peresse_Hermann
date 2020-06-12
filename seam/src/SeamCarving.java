5990c32f2ae7ac7956aace3b4f7d46c9a08883c4
blackjack-nix <theo.peresse-gourbil@edu.esiee.fr>
2020-06-12 18:50:30 +0200

    javadoc + arborescence

--- seam/src/SeamCarving.java
+++ seam/src/SeamCarving.java
@@ -4,7 +4,13 @@ import java.io.File;
 import java.lang.Math;
 
 
-
+/**
+* This class is used to resize (reduce) an image
+* Choice of pixels to remove depending on the luminescence of the pixel
+* @author Théo Peresse-Gourbil
+* @author Manon Hermann
+* @version 1
+*/
 public class SeamCarving {
 
 	//images declaration
@@ -20,6 +26,14 @@ public class SeamCarving {
 	private int[][] pathY;//horizontal path of the less important pixels
 
 
+	/**
+	* Constructor changes depending on mode :
+	* 	- 0 for the initialisation
+	* 	- x to reduce horizontally
+	* 	- y to reduce vertically
+	* @param img image that will be reduced
+	* @param mode choice of reduction mode
+	*/
 	public SeamCarving(final BufferedImage img, final String mode){
 		this.height = img.getHeight();
 		this.width = img.getWidth();
@@ -35,8 +49,7 @@ public class SeamCarving {
 		if(mode.equals("x")){
 			this.calculMx();
 			this.getPathX();
-		}
-		else if(mode.equals("y")){
+		} else if(mode.equals("y")){
 			this.calculMy();
 			this.getPathY();
 		} else if(mode.equals("0")){
@@ -45,9 +58,13 @@ public class SeamCarving {
 			System.out.println("error mode");
 			System.exit(1);
 		}
-
 	}
 
+
+	/**
+	*	set the double array in each (x,y) is the RGB value of pixel in coordonnates (x,y)
+	* @param inputImage The image which you xant to get the RGB array
+	*/
 	public void getrgbImage(final BufferedImage inputImage){
 		for(int i = 0; i < width;i++){
 			for(int j = 0; j < height;j++){
@@ -56,15 +73,12 @@ public class SeamCarving {
 		}
 	}
 
-	public static void printGrille(final int[][] grille){
-		for(int y=0; y < grille[0].length;y++){
-			for(int x=0; x < grille.length;x++){
-				System.out.print(grille[x][y]+" ");
-			}
-			System.out.println("");
-		}
-	}
 
+	/**
+	* Return the lunminescence of the rgb
+	* @param rgb the int in rgb format which you want to get the lunminescence
+	* @return luminescence
+	*/
 	private  int getLuminance(final int rgb){
 		int r = (rgb >>16 ) & 0xFF;
 		int g = (rgb >> 8 ) & 0xFF;
@@ -73,6 +87,13 @@ public class SeamCarving {
 		return gray;
 	}
 
+
+	/**
+	* Calcul the grdient of the (x,y) pixel with sobel methode
+	* @param x the x coordonnate
+	* @param y the y coordonnate
+	* @return gradient the gradient of the piexl
+	*/
 	private int gradient(int x, int y){
 		// Coordinates of 8 neighbours pixels
 		int px = x - 1;  // previous x
@@ -104,7 +125,11 @@ public class SeamCarving {
 		return norme;
 	}
 
-	public  void setEnergy(){
+
+	/**
+	* Set the energie for all the pixels of the image
+	*/
+	public void setEnergy(){
 		for (int y = 0; y < this.height ; y++){
 			for (int x = 0; x < this.width ; x++){
 				energyImage[x][y] = gradient(x,y);
@@ -112,12 +137,17 @@ public class SeamCarving {
 		}
 	}
 
+
+	/**
+	* Calcul the matrice Mx in which the bigger numbers are on the bottom
+	* @see calculMy()
+	*/
 	public void calculMx(){
 
-		//cas initial
+		// initial case
 		for(int x=0; x < Mx.length; x++){Mx[x][0]= energyImage[x][0];}
 
-		//cas général
+		// general case
 		for (int y = 1 ; y < Mx[0].length ; y++){
 
 			for(int x = 0 ; x < Mx.length; x++){
@@ -131,16 +161,21 @@ public class SeamCarving {
 		}
 	}
 
+
+	/**
+	*	Calculate a path to get the less important pixel in the matrice Mx
+	* @see getPathY()
+	*/
 	public void getPathX(){
 
 		pathX[Mx[0].length-1][0] = minLigne();
 		pathX[Mx[0].length-1][1] = Mx[0].length-1;
 
 		for (int y = Mx[0].length-1 ; y > 0 ; y--){
-			pathX[y-1][1] = y-1; //on met a jour le y
+			pathX[y-1][1] = y-1;  // update from y
 			int x_actual = pathX[y][0];
 
-			if (x_actual == 0){//cas ou on est tout a gauche
+			if (x_actual == 0){  // case where x is on the far left
 				if(Mx[x_actual][y-1] <= Mx[x_actual+1][y-1]){
 					pathX[y-1][0] = x_actual;
 				} else {
@@ -148,14 +183,13 @@ public class SeamCarving {
 				}
 			}
 
-			else if (x_actual == Mx.length -1){ //cas ou on est tout a droite
+			else if (x_actual == Mx.length -1){  // case where x is on the far rigth
 				if(Mx[x_actual][y-1] <= Mx[x_actual-1][y-1]){
 					pathX[y-1][0] = x_actual;
 				} else pathX[y-1][0] = x_actual-1;
 			}
 
-
-			else { //cas général
+			else {  //general case
 				if (Mx[x_actual][y-1] <= Mx[x_actual+1][y-1] && Mx[x_actual][y-1] <= Mx[x_actual-1][y-1]) {
 					pathX[y-1][0] = x_actual;
 				}
@@ -170,29 +204,39 @@ public class SeamCarving {
 
 	}
 
+
+	/**
+	* Calcul the matrce My in which the bigger numers are on the right
+	* @see calculMx()
+	*/
 	public void calculMy(){
-		//cas initial
+		// initial case
 		for(int y=0; y < My[0].length;y++){My[0][y] = energyImage[0][y];}
 
-		//cas général
+		// general case
 		for (int x = 1; x < My.length ; x++) {
 			for (int y = 0 ; y < My[x].length ; y++) {
-				if (y == My[0].length-1) My[x][y] = energyImage[x][y] + Math.min(My[x-1][y], My[x-1][y-1]);//tout en bas
-				else if (y == 0) My[x][y] = energyImage[x][y] + Math.min(My[x-1][y], My[x-1][y+1]);//tout en haut
-				else My[x][y] = energyImage[x][y] + Math.min(My[x-1][y-1], Math.min(My[x-1][y], My[x-1][y+1]));//cas general
+				if (y == My[0].length-1) My[x][y] = energyImage[x][y] + Math.min(My[x-1][y], My[x-1][y-1]);  // below
+				else if (y == 0) My[x][y] = energyImage[x][y] + Math.min(My[x-1][y], My[x-1][y+1]);  // up
+				else My[x][y] = energyImage[x][y] + Math.min(My[x-1][y-1], Math.min(My[x-1][y], My[x-1][y+1]));  // general case
 			}
 		}
 	}
 
+
+	/**
+	* Calculate a path to get the less important pixel in the matrice My
+	* @see getPathX()
+	*/
 	public void getPathY(){
-		pathY[My.length-1][0] = My.length-1; //x
-		pathY[My.length-1][1] = minColonne();  //y
+		pathY[My.length-1][0] = My.length-1; // x
+		pathY[My.length-1][1] = minColonne();  // y
 
 		for (int x= this.width-1; x > 0; x--){
 			pathY[x-1][0] = x-1;
 			int y_actual = pathY[x][1];
 
-			if (y_actual == 0){//cas ou on est tout en haut
+			if (y_actual == 0){  // case where x is on the far up
 				if(My[x-1][y_actual] <= My[x-1][y_actual+1]){
 					pathY[x-1][1] = y_actual;
 				} else {
@@ -200,7 +244,7 @@ public class SeamCarving {
 				}
 			}
 
-			else if (y_actual == this.height-1){//cas ou on est tout en bas
+			else if (y_actual == this.height-1){  // case where x is on the far down
 					if(My[x-1][y_actual] <= My[x-1][y_actual - 1]){
 						pathY[x-1][1]=y_actual;
 					} else {
@@ -208,7 +252,7 @@ public class SeamCarving {
 					}
 			}
 
-			else {//cas général
+			else {  // general case
 				if(My[x-1][y_actual] <= My[x-1][y_actual+1] && My[x-1][y_actual] <= My[x-1][y_actual -1]){
 					pathY[x-1][1] = y_actual;
 				}
@@ -222,6 +266,12 @@ public class SeamCarving {
 		}
 	}
 
+
+	/**
+	* Return the indice of the lowest number of the right column in the matrice My
+	* @return indice the indice of the lower
+	* @see minLigne()
+	*/
 	public int minColonne(){
 		int min = Integer.MAX_VALUE, indice = 0;
 		for(int y=0; y < height; y++){
@@ -233,35 +283,53 @@ public class SeamCarving {
 		return indice;
 	}
 
+
+	/**
+	* Return the minimal value of the bottom line in Mx
+	* @return indice the indice of the lower
+	* @see minColonne()
+	*/
 	public int minLigne(){
 		int min = Integer.MAX_VALUE, indice = -1;
 		for(int x = 0; x < width; x++){
 			if(Mx[x][Mx[0].length-1] < min){
-				min = Mx[x][Mx[0].length-1];//max
-				indice = x;//indice du max
+				min = Mx[x][Mx[0].length-1];  // maximum
+				indice = x;  //maximum index
 			}
 		}
 		return indice;
 	}
 
+
+ /**
+ * Remove a single path in the x direction
+ * @param img the image you want to get of a column
+ * @param pathx the path of the pixels you want to remove
+ * @see removeY()
+ */
 	public static BufferedImage removeX(final BufferedImage img, final int[][] pathx){
 		BufferedImage new_img = new BufferedImage(img.getWidth()-1, img.getHeight(),img.getType());
 		int xp=0;
-		for (int y = 0; y < new_img.getHeight();y++){
+		for (int y = 0; y < new_img.getHeight() -1;y++){
 			xp=0;
-			for( int x = 0; x < new_img.getWidth();x++){
+			for( int x = 0; x < new_img.getWidth() -1;x++){
 					if(xp == pathx[y][0]){
 						xp++;
 					}
 					new_img.setRGB(x,y,img.getRGB(xp,y));
-
 					xp++;
 			}
 		}
-
 		return new_img;
 	}
 
+
+ /**
+ * Remove a single path in the y direction
+ * @param img the image of the pixels you want to get of a line
+ * @param pathy the path you want to remove
+ * @see removeX()
+ */
 	public static BufferedImage removeY (final BufferedImage img, final int [][] pathy) {
 		BufferedImage new_img = new BufferedImage(img.getWidth(), img.getHeight()-1, img.getType());
 		int yp = 0;
@@ -274,12 +342,18 @@ public class SeamCarving {
 					new_img.setRGB(x,y,img.getRGB(x,yp));
 					yp++;
 			}
-		} // et si t'enleve celui du main XD ca va marcher c est sur. faut pas mettre getWidth -1 ? ET PAREIL POUR LA HAUTEUR c est ca !
+		}
 		return new_img;
 	}
 
+
+	/**
+	* Transorm a string in an Integer
+	* @param percentage the string you want convert int int
+	* @return int the int from the string
+	*/
 	public static int getPercentage(final String percentage){
-		String pa = percentage; //a modifier pour enlever tout ce qui n'est pas un chiffre
+		String pa = percentage;
 		int p = 100;
 		try {
 			p = Integer.parseInt(pa);
@@ -290,6 +364,11 @@ public class SeamCarving {
 		return p;
 	}
 
+
+	/**
+	* Open the image of the path and put in in inputImage
+	* @param path the path in your computer to open the file. It can bu relative of absolute
+	*/
 	public static void openImg(final String path){
 		try {
 			File inputFile = new File(path);
@@ -301,6 +380,11 @@ public class SeamCarving {
 		}
 	}
 
+
+	/**
+	* Main function
+	* @param args the arguments : path, %reductionX, %reductionY
+	*/
 	public static void main(String[] args){
 		if (args.length != 3){
 			System.out.println("Erreur : parametres incorrects. \n Format des parametres : chemin %reductionX %reductionY");
@@ -315,35 +399,24 @@ public class SeamCarving {
 
 		outputImage = inputImage;
 
-
-
 		for(int i=outputImage.getWidth(); i >= required_x; i--){
 			seam = new SeamCarving(seam.outputImage,"x");
 			outputImage = removeX(outputImage,seam.pathX);
 		}
-
 
 		for(int j=outputImage.getHeight();j >= required_y;j--){
 			seam = new SeamCarving(seam.outputImage,"y");
 			outputImage = removeY(outputImage,seam.pathY);
 		}
 
-
-
-
-
-
 		String extension = args[0].split("\\.")[1];
 
 		try {
 			File outputFile = new File(String.valueOf(required_x)+"x"+String.valueOf(required_y)+args[0]);
-			ImageIO.write(outputImage,extension.toUpperCase(),outputFile); //modifier pour metre l'image finale
+			ImageIO.write(outputImage,extension.toUpperCase(),outputFile);
 		} catch (Exception e){
 			System.out.println(e.toString());
 			System.exit(1);
 		}
-
-
-
 	}
 }
